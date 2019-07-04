cf449665c2d25f1c3a0a4a49ad0940ed246368dc
Spencer Williams <s@spencerenglish.com>
2019-07-03 18:26:35 -0700

    version bump

--- scripts/_ldflags.sh
+++ scripts/_ldflags.sh
@@ -1,6 +1,6 @@
 export GITHASH=`git rev-parse --short HEAD`
 export BUILDTIME=`date -u '+%Y-%m-%d_%I:%M:%S%p'`
-export VERSION="0.2.1-beta"
+export VERSION="0.2.2-beta"
 export LDFLAGS="
     -X github.com/spilliams/goraffe/internal/version.versionNumber=${VERSION}
     -X github.com/spilliams/goraffe/internal/version.gitHash=${GITHASH}
