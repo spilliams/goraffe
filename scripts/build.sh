1bc0a76ec11772c711586eef675a5b0c7c8541e3
Spencer Williams <s@spencerenglish.com>
2020-02-13 19:51:59 -0800

    scripts shouldn't assume GOPATH exists

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
-set -ex
+set -e
 
+o="."
+if [[ ! -z "$GOPATH" ]]; then
+    o=$GOPATH/bin
+fi
 source scripts/_ldflags.sh
-go build -ldflags "${LDFLAGS}" -o $GOPATH/bin/goraffe github.com/spilliams/goraffe/cmd/goraffe
+go build -ldflags "${LDFLAGS}" -o $o/goraffe github.com/spilliams/goraffe/cmd/goraffe
+echo "Updated binary $o/goraffe"
